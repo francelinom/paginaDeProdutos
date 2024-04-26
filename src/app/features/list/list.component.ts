ba7ab863965d5fb022059a612c0318be89447f0e
Francelino <francelino.silva.prestserv@petrobras.com.br>
2024-04-26 16:27:54 -0300

    feat: adiciona button de criar produto

--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -2,11 +2,13 @@ import { Component, inject } from '@angular/core';
 import { Product } from '../../shared/interfaces/product.interface';
 import { ProductsService } from '../../shared/services/products.service';
 import { CardComponent } from './components/card/card.component';
+import { RouterLink } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [CardComponent],
+  imports: [CardComponent, RouterLink, MatButtonModule],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
