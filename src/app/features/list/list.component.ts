59ba9855e28fc8739c09f7fefa42084071ec655c
Francelino <francelino.silva.prestserv@petrobras.com.br>
2024-04-26 16:02:40 -0300

    Feat: adiciona card de produtos

--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -1,11 +1,13 @@
 import { Component, inject } from '@angular/core';
 import { Product } from '../../shared/interfaces/product.interface';
 import { ProductsService } from '../../shared/services/products.service';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatButtonModule],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
